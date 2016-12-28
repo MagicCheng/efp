abfaee6362439f83d59eec0a74493b7b0af8c3ec
MagicCheng <569326782@qq.com>
2016-12-28 21:45:02 +0800

    end for 17

--- ex13.py
+++ ex13.py
@@ -2,10 +2,7 @@ from sys import argv
 
 script, first, second, third = argv
 
-first = raw_input ("How old are you?")
-second = raw_input ("How tall are you?")
-third = raw_input ("What are you doing?")
 print "The script is called:", script
-print "You are  %r years old.", first
-print "You are %r tall",  second
-print "You are %r",  third
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is", third
--- ex15.py
+++ ex15.py
@@ -1,14 +1,14 @@
 from sys import argv
 
-script, filename = argv
+script, filename = argv #unpack values two
 
-txt = open(filename)
+txt = open(filename) #ready for line 8
 
 print "Here's your file %r:" % filename
 print txt.read()
 
 print "Type the filename again:"
-file_again = raw_input(">")
+file_again = raw_input(">") #ready for line 13
 
 txt_again = open(file_again)
 
